245fd640dd9135213754ac99915d54f273289029
Rich Harris <richard.a.harris@gmail.com>
2014-05-01 10:38:41 -0400

    remove some unneeded code

--- src/interaction/mouseDrag.js
+++ src/interaction/mouseDrag.js
@@ -2,12 +2,8 @@ import clean from 'utils/clean';
 
 export default function draggable ( viewBox ) {
 	var svg,
-		dragging,
 		lastX,
-		lastY,
-		zoom,
-		activeFingers = [],
-		fingerById = {};
+		lastY;
 
 	svg = viewBox.svg;
 
@@ -15,21 +11,14 @@ export default function draggable ( viewBox ) {
 		// we don't care about right-clicks etc
 		if ( event.which !== undefined && event.which !== 1 ) return;
 
-		if ( viewBox._dirty ) clean( viewBox );
-
-		zoom = viewBox.getZoom();
 		lastX = event.clientX;
 		lastY = event.clientY;
-
-		dragging = true;
 
 		window.addEventListener( 'mousemove', mousemoveHandler, false );
 		window.addEventListener( 'mouseup', mouseupHandler, false );
 	}
 
 	function mousemoveHandler ( event ) {
-		if ( !dragging ) return;
-
 		viewBox.pan( ( event.clientX - lastX ), ( event.clientY - lastY ) );
 
 		lastX = event.clientX;
@@ -37,8 +26,6 @@ export default function draggable ( viewBox ) {
 	}
 
 	function mouseupHandler ( event ) {
-		dragging = false;
-
 		window.removeEventListener( 'mousemove', mousemoveHandler, false );
 		window.removeEventListener( 'mouseup', mouseupHandler, false );
 	}
