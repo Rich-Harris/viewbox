72e841ea039d7162de205f295f3826feb21a0c40
Rich Harris <richard.a.harris@gmail.com>
2014-05-14 17:44:27 -0400

    support for FF<=28

--- src/utils/clean.js
+++ src/utils/clean.js
@@ -1,5 +1,7 @@
+var percentage = /^(\d)+%$/;
+
 export default function clean ( viewBox ) {
-	var computedStyle;
+	var computedStyle, parentElement, parentComputedStyle, parentWidth, parentHeight;
 
 	viewBox._elWidth = viewBox.svg.offsetWidth;
 	viewBox._elHeight = viewBox.svg.offsetHeight;
@@ -7,8 +9,30 @@ export default function clean ( viewBox ) {
 	if ( viewBox._elWidth === undefined ) {
 		// We must be in FireFox. Goddammit.
 		computedStyle = getComputedStyle( viewBox.svg );
-		viewBox._elWidth = stripPx( computedStyle.width );
-		viewBox._elHeight = stripPx( computedStyle.height );
+
+		if ( percentage.test( computedStyle.width ) || percentage.test( computedStyle.height ) ) {
+			// We must be in an old version of FireFox. AARGH!
+			parentElement = viewBox.svg.parentElement;
+			parentComputedStyle = getComputedStyle( parentElement );
+
+			parentWidth = stripPx( parentComputedStyle.width );
+			parentHeight = stripPx( parentComputedStyle.height );
+
+			// TODO 'auto' etc
+
+			if ( percentage.test( computedStyle.width ) ) {
+				viewBox._elWidth = +( computedStyle.width.replace( '%', '' ) ) * parentWidth / 100;
+			}
+
+			if ( percentage.test( computedStyle.height ) ) {
+				viewBox._elHeight = +( computedStyle.height.replace( '%', '' ) ) * parentHeight / 100;
+			}
+		}
+
+		else {
+			viewBox._elWidth = stripPx( computedStyle.width );
+			viewBox._elHeight = stripPx( computedStyle.height );
+		}
 	}
 
 	viewBox._aspectRatio = viewBox._elWidth / viewBox._elHeight;
